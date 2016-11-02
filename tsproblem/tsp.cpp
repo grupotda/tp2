f3c927e07ef71d791bf202f7695873ba4a8bc306
chrishaig14 <chrishaig14@gmail.com>
2016-11-01 23:34:38 -0300

    Creo archivos con las matrices para algunos problemas

--- tsproblem/tsp.cpp
+++ tsproblem/tsp.cpp
@@ -63,74 +63,51 @@ pair<vector<pair<int, int> >, int> held_karp(vector<vector<int> > c, int v, vect
     return make_pair(path, min);
 }
 
-int main()
-{
-
-    ifstream file("p01.tsp");
-    string line;
-    int k = 0;
-    vector<vector<int> > c;
-    while (getline(file, line)) {
-        if (line == "EDGE_WEIGHT_SECTION") {
-            while (getline(file, line) && k < 15) {
-               vector<string> row_str = split(line, ' ');
-               vector<int> row;
-                for (int i = 0; i < row_str.size(); i++) {
-                    if (row_str[i] != "") {
-                        row.push_back(atoi(row_str[i].c_str()));
-                    }
-                }
-                for (int j = 0; j < row.size(); j++) {
-                    cout << row[j] << " ";
-                }
-                c.push_back(row);
-                k++;
-                cout << endl;
-            }
-        }
-    }
-    vector<int> s;
-    for (int i = 1; i < 15; i++) {
-        s.push_back(i);
-    }
-    map<pair<int, vector<int> >, pair<vector<pair<int, int> >, int> > previous;
-
-pair<vector<pair<int, int> >, int> path_cost = held_karp(c, 0, s, previous);
-   cout << "path: " << endl;
-
-    for (int i = 0; i < path_cost.first.size(); i++) {
-        cout << path_cost.first[i].first << "-> " << path_cost.first[i].second << " : " << c[path_cost.first[i].first][path_cost.first[i].second] << endl;
-    }
-
-    cout << "cost:" << path_cost.second << endl;
-    cout << "path from file: " << endl;
-
-    vector<int> p;
-    p.push_back(1);
-    p.push_back(13);
-    p.push_back(2);
-    p.push_back(15);
-    p.push_back(9);
-    p.push_back(5);
-    p.push_back(7);
-    p.push_back(3);
-    p.push_back(12);
-    p.push_back(14);
-    p.push_back(10);
-    p.push_back(8);
-    p.push_back(6);
-    p.push_back(4);
-    p.push_back(11);
-    p.push_back(1);
-
-    for (int j = 0; j < p.size(); j++) {
-        p[j] -= 1;
-    }
-    int tt = 0;
-    for (int j = 0; j < p.size(); j++) {
-        cout << p[j] << "-> " << p[j + 1] << " : " << c[p[j]][p[j + 1]] << endl;
-        tt += c[p[j]][p[j + 1]];
-    }
-    cout << "cost from file: " << tt << endl;
-
+vector < vector <int > > loadMatrix(char* filename){
+		ifstream file(filename);
+		string row;
+		vector< vector<int> > m;
+		int i = 0;
+		while(getline(file,row)){
+			vector<string> row_str = split(row,' ');
+			m.push_back(vector<int>());
+			for(int j = 0; j < row_str.size(); j++){
+					m[i].push_back(atoi(row_str[j].c_str()));
+			}
+			i++;
+		}
+return m;
 }
+
+int main(int argc, char* argv[])
+{	for(int i = 1; i < argc; i++){
+		vector< vector<int> > m = loadMatrix(argv[i]);
+		cout << "size = " << m.size() << endl;
+		cout << endl;
+		for(int j = 0; j < m.size(); j++){
+			for(int k = 0; k < m.size(); k++){
+				cout << m[j][k] << " ";
+			}
+			cout << endl;
+		}
+		cout << endl;
+		vector<int> s;
+		for(int j = 1; j < m.size(); j++){
+			s.push_back(j);
+		}
+		map<pair<int, vector<int> >, pair<vector<pair<int, int> >, int> >  previous;
+		pair<vector<pair<int, int> >, int> result = held_karp(m,0,s,previous);
+		vector<pair<int, int> > path = result.first;
+		cout << "path: " << endl;
+		for(int j = 0; j < path.size(); j++){
+			cout << path[j].first + 1 << endl;
+		}
+		cout << path[path.size()-1].second + 1 << endl;
+		cout << endl;
+		cout << "cost: " << result.second << endl; 
+	}
+}
+
+
+
+
